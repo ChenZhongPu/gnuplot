298a5c0e2ce173e63adcf25a69aa6b47b30efbaf
CHEN zhongpu <chenloveit@gmail.com>
2022-04-24 09:42:37 +0800

    add gitignore for deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 cd book
 git init
 git branch -M pages
+touch .gitignore
+echo ".DS_Store" >> .gitignore
 git add .
 git commit -m 'deploy'
 git remote add origin git@github.com:ChenZhongPu/gnuplot.git
